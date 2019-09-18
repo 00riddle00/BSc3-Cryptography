fa763e2e336ef08f2dd404de013d5e4ce8bfd1a4
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-09-18 13:04:34 +0300

    .

--- prat3.py
+++ prat3.py
@@ -40,6 +40,70 @@ NĘAGY IYĘŪS CĄUCD FTŪĘI TSĮAL
 ŲĖDDH LCVKK LŪNSU IIE
 '''
 
+sifr3 = u'''
+FĮLPI ŽHVFB PFOHY ŽCFĘE SĮDCČ 
+ROKCF BĄROY CTŠSK LYLHI OZFZY 
+GŠŽBG CTDBD YNCVK LCŲTC HDBĖB 
+AĘLĮŽ ŲĮEYĘ DBĄRĮ LHŠFP BČRŪŠ 
+ŠKŠAK ĘURĄĖ UGĄTV ĄIRIŽ MŠGIY 
+FVMOČ RLSJN HIBIV ĮFŠPK ŪLOĮD 
+CĘOKD GEYBG OCŽŽF ŲUCYG IĄEYK 
+SJĄPB LKĮTČ DZFHO TZNŲT CSDFF 
+FEHYF IĘŠUĮ DĖMYČ HCSDV PYĮHE 
+ĮFPFŽ LHŪID ĮBŪSH IICGH ĘGŠJĮ 
+LLYDG ŽIETZ KŪFEE VTŽĄH RLFCL 
+ŪĄYEK CŽCMR KYKĘZ PBUKY DCYGF 
+SLDBV ĖESLR IĄEMŲ CĮDĄH YĘSHY 
+PIĘIS KĄĄIŽ SFTCD GFSGŽ DMRKĘ 
+CIRPB EJFĄD CJUYĘ IUŽRL ĘSŠŪF 
+EĘFSŠ ĮYKMŲ CUVPK GHĘJM GYĘIS 
+KŠEŪĮ ĘCTEĄ IJSĘĄ VĄROE ČŲEMG 
+ĮVKNČ TBUYO DLJDA ĮSZIJ JUFYT 
+EIĮNC ĘGASK NVĖED FEĘĖV IRMSN 
+SĮKŠĄ YJHID MHBUL YDČGM LKŠYI 
+ONAĖĄ DIJHA ĮEEĄH GHSGC ĮBFEN 
+ĮDIĄI ULŪĮŽ JGFSG VŠRĖO ĮPDŽJ 
+MGLSY DHROĘ KUCPK HŽĖĘL OBYJO 
+DMISG SYHIĖ ŲUĮHK ŠDGŽŽ ĖZAĘB 
+OHYDF ĄĮYĘC TŠIFE VĘTŠI ŠAKĘĮ 
+ŪĄRĘĮ SIŪPĮ JSĘGŠ JNOYC UCĘKH 
+ĘFŽŽP ĮYĖCT BĄĮŪL OĮIYI UYKTC 
+DGĮVJ DZYRL FVMĘG IEVJH IĖKTĄ 
+CHCYR ĮKIGI ĖŪNSY NŪPĮE SIGŠĖ 
+EŽOCK IJĘAY ĮTEUG ĮŠCJH CŲKŠI 
+ŽBDŪN LĮDII ŲAJCH DIKSC ĮLEĘĖ 
+AKMYB ĄŪFHY IMSKS HIJŠP INŽEA 
+CČGGY ĘĮCĘB ĮCYGI ĄNUYS IIĄĮH 
+SCFIĄ ĮČSFĮ FFYAI ĖDAYŪ YĮCĖI 
+DRŽŪO DRJŠP ROTŠŽ PŽRCO ĄERĮV 
+KĮCŲF AĄSGŠ PJLCĮ TCYBČ DOĘYV 
+ŠOOHG IĄEYF STŪIB YFSMA DBPIĖ 
+TŽŲJP HĮŲŠY GLTVY IEBYO HKŪŲB 
+ZCSKŠ UZHĄĮ TFĄJU UCIŠK ĮSDĮT 
+FĄĖPĘ DGIĖU EMTŽC ĄĮOĘS IDUNE 
+VFKIJ ĘBFSY VĄFGO CŽŽPE EKŠ  
+'''
+
+sifr4 = u'''
+ĘDSFA ĮNŪTC VĄNPJ COUNI ŽSĘAH 
+ŠLKĘS VZGHŪ ĮFČBV ŪACCI FYSŪC 
+ŽBCĖŪ RDŲCŽ REDMJ ŽHSLD ŽČĖCĖ 
+DTPYU ZDNUB ĮŽLFT KMPMZ ŽEBŲH 
+ŠPMBM KZĮZO HĘŠGG GCĄGŪ KCZRA 
+ĖRMMĮ NYŠIJ ĄĖČČI DUKNH COYAG 
+ŲSHAI VĘYŪA ŠUĘDŪ NŠEZR ŪŽĖEG 
+ĮEŽUU ĘĄZDJ HNPSH CĄŽČS ŲCEČĄ 
+CDVĖŽ AADFM ZDŽLČ ĘJDMS VŲEHD 
+VĘŲSS DRZĘĮ SŪJGL ŲOGLM NPLZB 
+MŽDDK BMJŪL ČOMĖK ĄKJĘT KORĖB 
+ĮERBĄ RČĖŪS ĄĘKŪT HŠSRĖ VSŠYB 
+ĮDĖBČ BGLĄB ĘYVVŪ TČŪNV TDRSC 
+HGDFŲ JEIHA ĘEAŪH NAHVĖ LDSHS 
+TTSSA ĖDIĘO IYŠIS ADJ   
+'''
+
+sifr4_raktas = u'KLEVAS'
+
 
 def prepare(text):
     text=text.upper()
@@ -95,7 +159,120 @@ def friedmann_test(sifr_rakto_dalis):
 # UZD4
 
 
+## Dazniai
+from collections import defaultdict
+abc=unicode('AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ','utf-8')
+def freq(text):
+    d = defaultdict(int)
+    s=""
+    for w in text:
+        if w in abc:
+            d[w] += 1                   
+    for w in sorted(d, key=d.get, reverse=True):
+        s+=w
+    return s
 
+t='TRRyyyIIU'
+print(freq(t))
+
+'AEIO'
+
+## Friedmann test
+def friedm(text,k):
+    textn=unicode("",'utf-8')
+    for r in text:
+        if r in abc:
+            textn+=r
+    l=len(textn)
+    s=0
+    for i in range(k,l):
+        if textn[i]==textn[i-k] :
+            s+=1
+    return  1.*s/(l-k)  
+t=unicode('PIGĄU KCLĘR ŽSOPĘ CKCIŲ ĖGACY ','utf-8')
+print(friedm(t,2))
+
+from collections import defaultdict
+abc=unicode('AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ','utf-8')
+n=len(abc)
+
+## Raktui speti
+def guess(test, k, sifr): #test - dažniausių raidžių eilutė, k - spėjamas šifro raktas
+    tst=unicode('','utf-8')
+    for r in test:
+        if r in abc:
+            tst+=r    
+    tstk=unicode('','utf-8')
+    for r in tst:
+        tstk+=abc[(abc.index(r)+k)%n]
+    d = defaultdict(int)
+    sifrn=unicode('','utf-8')
+    for r in sifr:
+        if r in abc: sifrn+=r
+    for r in sifrn:
+        if r in tstk: d[r]+=1
+    kiek=len(sifrn)
+    s=0
+    for a in d.keys(): s+=d[a]
+    return s/kiek
+test=unicode('ABC','utf-8')
+sifr=unicode('ABABABASSDDHHDKKKCCCCCLKLDKSJJSJSJLL','utf-8')
+
+guess(test,2,sifr)
+
+## SKAIDYMAS
+abcu='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+n=len(abcu)
+def prepare2(text): #remove non-ascii
+    textn=''
+    for a in text:
+        if a in abcu:
+            textn+=a
+    return textn.upper()
+
+
+def split(text,d):
+    textn=prepare2(text)
+    tspl=['']*d
+    n=len(textn)
+    for i in range(0,n):
+        tspl[i%d]+=textn[i]
+    return tspl    
+    
+f=split('ABABABABABAB',2)
+f[0],f[1]
+
+def Vigenere2(text,key): #Vigenere cipher
+    textn=prepare(text)
+    keyn=prepare(key)
+    textc=u""
+    keys=[]
+    lk=len(keyn)
+    for i in range(0,lk):
+        keys.append(abc.index(keyn[i]))
+    lt=len(textn)
+    for i in range(0,lt):
+        textc+=abc[(abc.index(textn[i])+keys[i%lk])%n]
+    return textc
+
+sifr4 = clean_text(sifr4)
+
+n = len(sifr4_raktas)
+viso_text_ilgis = len(sifr4)
+galut_res = u''
+
+print(viso_text_ilgis)
+
+
+for i in range(n,viso_text_ilgis,n):
+    #print(i)
+    continue
+    res = Vigenere2(sifr4[n-6:n], sifr_to_desifr(sifr4_raktas))
+    sifr4_raktas = res
+    for a in res:
+        galut_res+=a
+
+print(galut_res)
 
 
 

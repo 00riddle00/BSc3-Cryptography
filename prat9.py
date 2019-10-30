2d2f7bf1caf82c014071cfe693ce19179b750a80
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-30 19:05:03 +0200

    .

--- prat9.py
+++ prat9.py
@@ -1,5 +1,7 @@
 getBin = lambda x, n: x >= 0 and str(bin(x))[2:].zfill(n) or "-" + str(bin(x))[3:].zfill(n)
+
 abc_h='ABCDEFGHIJKLMNOP' # h-funcijos reikšmių abėcėlė
+
 def e(m,k): #Kriptosistema
     f=lambda u,v,w:u^^((v*v+77*w)%256)
     c=[m[1],f(m[0],m[1],k[0])]
@@ -19,7 +21,6 @@ def decod(s): #h-funkcijos reikšmė į bitus
         if c in abc_h:
             b+=bt(abc_h.index(c))
     return b
-
 
 # maisos fjos skaiciavimo script pgl cbc rezima
 def hfCBC(h0,r,s): # h-funkcija CBC rezimas
@@ -47,69 +48,67 @@ def hfOFB(h0,r,s): # h-funkcija OFB rezimas
         h=e(m,k)
     return code(m)
 
-# blokas is 2ju skaiciu arba 2 raidziu blokas 'ss' (2 baitai)
-h0='uo' # iv
-r='pi' # kriptosistemos raktas
-s='uogapiktas' # s yra pranesimas, kuri norime xorinti
-h=hfCBC(h0,r,s)
-print h, decod(h)
-# h yra hashas, atitinka 2 bytes
-# decod(h) gauname h bitukais (16bits)
+# custom fja dvi raides paversti i binary
+def getBin2(pair):
+    return getBin(ord(pair[0]), 8) + getBin(ord(pair[1]), 8)
 
-
-#h=hfOFB(h0,r,s)
-#print h, decod(h)
-
-#Sudarykite II tekstą M_1= tomgieABpiktas, kad būtų h(M)=h(M_1),
-
-S = 'uogapiktas'
+# UZD1
+# zodis: 'uogapiktas'
 M1 = 'uo'
 M2 = 'ga'
 M3 = 'pi'
 M4 = 'kt'
 M5 = 'as'
 
-#h0='uo' # iv'
-h0 = 'uo'
+h0 = 'uo' # init vektorius
 r='pi' # kriptosistemos raktas
-s=M1 # s yra pranesimas, kuri norime xorinti
-h=hfCBC(h0,r,s)
-print h, decod(h)
-# decod - savo israiska decode, 16 raidziu abecele, 1 raide - 4 bitais
+s=M1 # s yra pranesimas, kuri norime xorinti. Blokas is 2ju skaiciu arba 2 raidziu, ex. blokas 'ss' (2 baitai)
+h=hfCBC(h0,r,M1)
+#print h, decod(h) # h yra hashas, atitinka 2 bytes
+#print decod(h) # decod(h) gauname h bitukais (16bits)
+# decod - destytojo israiska decode, 16 raidziu abecele, 1 raide - 4 bitais
 
 h_M1 = decod(h)
+M2_bin = getBin2(M2)
 
-def getBin2(pair):
-    return getBin(ord(pair[0]), 8) + getBin(ord(pair[1]), 8)
+print "h_M1=", h_M1
+print "M2_bin=", M2_bin
 
-print type(h_M1)
-print type(getBin2(M2))
-
-y = bin(int(h_M1,2) ^^ int(getBin2(M2),2))
+y = int(h_M1,2) ^^ int(M2_bin,2) # y = h_M1 ^^ M2
+print "y=", y
 
 N1 = 'to'
 N2 = 'mg'
 N3 = 'ie'
 
-h=hfCBC(h0,r,'tomg')
-print h, decod(h)
+h=hfCBC(h0,r,N1+N2) # veiktu ir su N1+N2+N3
+#print h, decod(h)
 
 h_N1N2 = decod(h)
+h_N1N2_int = int(h_N1N2,2)
 
-print type(y)
-print type(h_N1N2)
+print "h_N1N2_int=", h_N1N2_int
 
-X = bin(int(y,2) ^^ int(h_N1N2,2))
+X = y ^^ h_N1N2_int
+
+X = bin(X)
+print "X=", X
 
 X = X[2:]
+print "X=", X
 
-x_1 = chr(int(X[0:8],2))
-x_2 = chr(int(X[8:16],2))
+x1 = chr(int(X[0:8],2))
+x2 = chr(int(X[8:16],2))
 
-print x_1, x_2
+print "x1=", x1
+print "x2=", x2
+print "ord(x1)=", ord(x1) # surandame google kas per ASCII simbolis tai yra
+print "ord(x2)=", ord(x2) # surandame google kas per ASCII simbolis tai yra
 
-h1 = hfCBC(h0, r, 'uogapiktas')
-h2 = hfCBC(h0, r, 'tomg'+x_1 + x_2 + 'iepiktas')
+h_M = hfCBC(h0, r, M1+M2+M3+M4+M5)
+h_M1 = hfCBC(h0, r, N1+N2 + x1 + x2 + M3+M4+M5) # veiktu ir su N1+N2+N3
 
-print h1
-print h2
+print "h_M=", h_M
+print "h_M1=", h_M1
+
+assert(h_M == h_M1)

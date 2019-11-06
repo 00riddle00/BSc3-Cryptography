d2ffa67c47445fc4e0b8211e21f74dc2b0eabb37
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-06 12:15:33 +0200

    .

--- tmp.py
+++ tmp.py
@@ -1,115 +1,33 @@
-getBin = lambda x, n: x >= 0 and str(bin(x))[2:].zfill(n) or "-" + str(bin(x))[3:].zfill(n)
-abc_h='ABCDEFGHIJKLMNOP' # h-funcijos reikšmių abėcėlė
-def e(m,k): #Kriptosistema
-    f=lambda u,v,w:u^^((v*v+77*w)%256)
-    c=[m[1],f(m[0],m[1],k[0])]
-    return [c[1],f(c[0],c[1],k[1])]
+def super_incr(mn, m): # Generating superincreasing weights
+    w=[]
+    w.append(int(random()*mn+mn))
+    p=w[0]
+    for i in range (0,m-1):
+        v=p+int(random()*mn+mn)
+        w.append(v)
+        p=p+v
+    return [w,p]
 
-def code(m): 
-    e=getBin(m[0],8)+getBin(m[1],8)
-    h=''
-    for i in range(0,4):
-        h+=abc_h[int(e[4*i:4*i+4],2)]
-    return h
+exmpl=super_incr(5,10)
+exmpl
+p=exmpl[1]+1
+p
+s=3777 #for example
+gcd(p,s) # greatest common divisor
+V=[]
+for w in exmpl[0]: # create the public key
+    V.append(w*s%p)
 
-def decod(s): #h-funkcijos reikšmė į bitus
-    bt=lambda r: bin(16^^r)[3:]
-    b=''
-    for c in s:
-        if c in abc_h:
-            b+=bt(abc_h.index(c))
-    return b
+#Viešas raktas:
+PUB = [33415, 35218, 230274, 69475, 189589, 82664, 73209, 185458]
 
-
-# maisos fjos skaiciavimo script pgl cbc rezima
-def hfCBC(h0,r,s): # h-funkcija CBC rezimas
-    h=[ord(h0[0]),ord(h0[1])]
-    k=[ord(r[0]),ord(r[1])]
-    l=len(s)
-    if l%2==1:
-         s=s+s[0]
-         l+=1
-    for i in range(0,l//2):
-        m=[ord(s[2*i])^^h[0],ord(s[2*i+1])^^h[1]]
-        h=e(m,k)
-    return code(h)
-
-def hfOFB(h0,r,s): # h-funkcija OFB rezimas
-    h=[ord(h0[0]),ord(h0[1])]
-    k=[ord(r[0]),ord(r[1])]
-    l=len(s)
-    if l%2==1:
-         s=s+s[0]
-         l+=1
-    h=e(h,k)    
-    for i in range(0,l//2):
-        m=[ord(s[2*i])^^h[0],ord(s[2*i+1])^^h[1]]
-        h=e(m,k)
-    return code(m)
-
-# blokas is 2ju skaiciu arba 2 raidziu blokas 'ss' (2 baitai)
-h0='uo' # iv
-r='pi' # kriptosistemos raktas
-s='uogapiktas' # s yra pranesimas, kuri norime xorinti
-h=hfCBC(h0,r,s)
-print h, decod(h)
-# h yra hashas, atitinka 2 bytes
-# decod(h) gauname h bitukais (16bits)
+# Pirma privataus rakto komponentė
+PKEY_01 = 1715
 
 
-#h=hfOFB(h0,r,s)
-#print h, decod(h)
+# Modulis:
+MD = 267835
 
-#Sudarykite II tekstą M_1= tomgieABpiktas, kad būtų h(M)=h(M_1),
+#šifras:
+C = [408176, 603089, 796412, 593634, 713748, 230274, 408176, 796412, 606823, 533614, 593634, 230274, 490840, 450950, 593634, 450950, 408176, 450950]
 
-S = 'uogapiktas'
-M1 = 'uo'
-M2 = 'ga'
-M3 = 'pi'
-M4 = 'kt'
-M5 = 'as'
-
-#h0='uo' # iv'
-h0 = 'uo'
-r='pi' # kriptosistemos raktas
-s=M1 # s yra pranesimas, kuri norime xorinti
-h=hfCBC(h0,r,s)
-print h, decod(h)
-# decod - savo israiska decode, 16 raidziu abecele, 1 raide - 4 bitais
-
-h_M1 = decod(h)
-
-def getBin2(pair):
-    return getBin(ord(pair[0]), 8) + getBin(ord(pair[1]), 8)
-
-print type(h_M1)
-print type(getBin2(M2))
-
-y = bin(int(h_M1,2) ^^ int(getBin2(M2),2))
-
-N1 = 'to'
-N2 = 'mg'
-N3 = 'ie'
-
-h=hfCBC(h0,r,'tomg')
-print h, decod(h)
-
-h_N1N2 = decod(h)
-
-print type(y)
-print type(h_N1N2)
-
-X = bin(int(y,2) ^^ int(h_N1N2,2))
-
-X = X[2:]
-
-x_1 = chr(int(X[0:8],2))
-x_2 = chr(int(X[8:16],2))
-
-print x_1, x_2
-
-h1 = hfCBC(h0, r, 'uogapiktas')
-h2 = hfCBC(h0, r, 'tomg'+x_1 + x_2 + 'iepiktas')
-
-print h1
-print h2

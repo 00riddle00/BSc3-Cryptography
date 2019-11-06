875da949b4ad6170d0985632cf650bdaf4d87a7b
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-06 12:48:26 +0200

    .

--- tmp.py
+++ tmp.py
@@ -36,20 +36,61 @@ w1 = W[0]
 v1 = V[0]
 
 
-d = gcd(v1, p)
 
-print 'dbd', dbd
-ti = (w1/d) / (v1/d) % (p/d)
-
-res = []
-
-if d == 1:
-    print "d=1"
-    print "ti=", ti
-else:
-    for i in range(1,d):
-        ti = ti + i*(p/d)
+def getT(vi, wi):
+    d = gcd(vi, p)
+    ti = (wi/d) / (vi/d) % (p/d)
+    res = []
+    if d == 1:
+        print "d=1"
+        return [ti]
+    else:
         res.append(ti)
-    print "d=", d
-    print "ti_LIST=", res
+        for i in range(1,d):
+            ti = ti + i*(p/d)
+            res.append(ti)
+        print "d=", d
+        return res
 
+res = getT(v1, w1)
+
+print "res=", res
+
+for ti in res:
+    W = [elem * ti % p for elem in V]
+    print "ti=", ti, "W=", W
+    
+W = [1715, 2048, 4204, 8375, 16744, 33539, 66999, 134078]
+
+t = 96706
+#t = 364541
+
+CR = [0,0,0,0,0,0,0,0]
+
+for i in range(8):
+    print i
+    CR[i] = t * C[i] % p
+    
+print "CR=", CR
+
+W1 = [0,0,0,0,0,0,0,0]
+
+CR1 = CR[0]
+
+#def get_WI():
+#    for i in range(
+#    tmp = CR1 - W[7]
+#    if tmp >= 0:
+#        CR1 = tmp
+        
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

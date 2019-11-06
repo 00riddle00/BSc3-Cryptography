6918c8bedbeb197dd2e60e508274d5ec973e18bc
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-06 12:28:18 +0200

    .

--- tmp.py
+++ tmp.py
@@ -19,15 +19,37 @@ for w in exmpl[0]: # create the public key
     V.append(w*s%p)
 
 #Viešas raktas:
-PUB = [33415, 35218, 230274, 69475, 189589, 82664, 73209, 185458]
+V = [33415, 35218, 230274, 69475, 189589, 82664, 73209, 185458]
 
 # Pirma privataus rakto komponentė
-PKEY_01 = 1715
+W = [1715, 0, 0, 0, 0, 0, 0, 0]
 
 
 # Modulis:
-MD = 267835
+p = 267835
 
 #šifras:
 C = [408176, 603089, 796412, 593634, 713748, 230274, 408176, 796412, 606823, 533614, 593634, 230274, 490840, 450950, 593634, 450950, 408176, 450950]
+
+
+w1 = W[0]
+v1 = V[0]
+
+
+d = gcd(v1, p)
+
+print 'dbd', dbd
+ti = (w1/d) / (v1/d) % (p/d)
+
+res = []
+
+if d == 1:
+    print "d=1"
+    print "ti=", ti
+else:
+    for i in range(1,d):
+        ti = ti + i*(p/d)
+        res.append(ti)
+    print "d=", d
+    print "ti_LIST=", res
 

d9ac779074f4b47399fc8a74043dd912b3bfc579
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-22 10:14:02 +0200

    .

---
+++ prat11.py
@@ -0,0 +1,28 @@
+# Tekstams versti skaičiais (ir atvirkščiai)
+A='abcdefghijklmnopqrstuvwxyz'
+def i_skaiciu(text):
+    t=''
+    for r in text:
+        if r in A:
+            ind=A.index(r)+1
+            if ind<10: t=t+'0'+str(ind)
+            else: t=t+str(ind)
+    return int(t,10)  
+
+def i_teksta(M):
+    n=M
+    text=''
+    while n>0:
+        ind=n%100
+        ind=ind-1
+        if (ind>=0) & (ind<len(A)):
+            text+=A[ind]
+            n=(n-ind+1)//100
+        else:
+            text+='?'
+            n=(n-ind+1)//100            
+    return text[::-1]     
+    
+M=i_skaiciu('vienas')
+T=i_teksta(M)
+M,T

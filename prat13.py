0c1883d1a0690567c638fe3ae191e9609552fe2f
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-29 10:16:56 +0200

    .

--- prat13.py
+++ prat13.py
@@ -1,37 +1,67 @@
+# Tekstams versti skaičiais (ir atvirkščiai)
+A='abcdefghijklmnopqrstuvwxyz'
+def i_skaiciu(text):
+    t=''
+    for r in text:
+        if r in A:
+            ind=A.index(r)+1
+            if ind<10: t=t+'0'+str(ind)
+            else: t=t+str(ind)
+    return int(t,10)  
 
-#Blumo-Goldwasserio šifro privatus raktas
-K_pr=[172764355471, 535707589267]
+def i_teksta(M):
+    n=M
+    text=''
+    while n>0:
+        ind=n%100
+        ind=ind-1
+        if (ind>=0) & (ind<len(A)):
+            text+=A[ind]
+            n=(n-ind+1)//100
+        else:
+            text+='?'
+            n=(n-ind+1)//100            
+    return text[::-1]     
+    
+#M=i_skaiciu('vienas')
+#T=i_teksta(M)
 
-#Šifras: 
-C = [126, 33, 250, 155, 124, 5, 162, 85, 163, 212, 6, 246, 158, 15, 33, 25826257355237703374115]
 
-p = K_pr[0]
-q = K_pr[1]
-x = C[-1]
-n = p*q
+#Vieši Algio ElGamalio  schemos raktai
+[p,alpha,beta]=[12016012609141909200527091927191118250205121973, 1340, 567229645341541746447797908693212382352016288]
 
-t = len(C) - 1
+#Algio parašai
+# tekstas_1
+t1="naujienos"
+x1=140121100905141519
+# Parašas
+[gama1,delta1] = [5570393885818468990720736511935245441421473683, 11318193604278953165137689101864251625102398263]
 
-e = power_mod( (p+1)/4, t+1, p-1)
-d = power_mod( (q+1)/4, t+1, q-1)
+#tekstas_2
+t2 = "lektuvas veluos"
+x2=120511202122011927220512211519
+# Parašas
+[gama2,delta2] = [5570393885818468990720736511935245441421473683, 6600840420895214539082508270820765392182440243]
+ 
+# Šifruotas laiškas Algiui 
+[C_1,C_2]= [5353434162946175342506864580712183347054386024, 3415125406315829810122511480055610251790431991]
 
-u = power_mod(x, e, p)
-v = power_mod(x, d, q)
 
-s = (1/p) % q
-r = (1/q) % p
+p=next_prime(12^18)
+g=primitive_root(p)
 
-x0 = (u*(r*q) + v*(s*p)) % n
+a=199111
+bt=power_mod(g,a,p)
+K_pr=a
+K_v=[p,g,bt]
 
-res = []
+# K_pr, K_v
 
-xi = x0
-for i, ci in enumerate(C[:-1]):
-    # if i = 0, then xi = x1, ci = c1, pi = p1 (just random explanation)
-    xi = power_mod(xi, 2, n)
-    ci = C[i]
-    pi = xi % 256
-    mi = ci^^pi
-    res.append(chr(mi))
-
-print ''.join(res)
+M=10000000000001
+k=12345
+C_1=power_mod(g,k,p)
+C_2=M*power_mod(bt,k,p)%p
+C=[C_1,C_2]
+#C
+M=C_2/power_mod(C_1,a,p)%p
+M

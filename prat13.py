52710c2745e6ff0075bd066c159829e0a43175c7
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-29 10:58:12 +0200

    .

--- prat13.py
+++ prat13.py
@@ -42,26 +42,66 @@ t2 = "lektuvas veluos"
 x2=120511202122011927220512211519
 # Parašas
 [gama2,delta2] = [5570393885818468990720736511935245441421473683, 6600840420895214539082508270820765392182440243]
- 
+
 # Šifruotas laiškas Algiui 
 [C_1,C_2]= [5353434162946175342506864580712183347054386024, 3415125406315829810122511480055610251790431991]
 
+#gama1=gama2
+gama = gama1
 
-p=next_prime(12^18)
-g=primitive_root(p)
+#tikriname parasus
+#parasas1
+print(power_mod(beta,gama,p)*power_mod(gama,delta1,p)%p == power_mod(alpha,x1,p))
+#parasas2
+print(power_mod(beta,gama,p)*power_mod(gama,delta2,p)%p == power_mod(alpha,x2,p) %p)
 
-a=199111
-bt=power_mod(g,a,p)
-K_pr=a
-K_v=[p,g,bt]
+d=gcd(delta1-delta2,p-1)
 
-# K_pr, K_v
+def get_ki(d):
+    
+    k0 = (x1-x2)/(delta1-delta2) % ((p-1)/d)
+    ki = k0
 
-M=10000000000001
-k=12345
-C_1=power_mod(g,k,p)
-C_2=M*power_mod(bt,k,p)%p
-C=[C_1,C_2]
-#C
+    if d == 1:
+          return k0
+    
+    while power_mod(alpha,ki,p)%p != gama:
+        ki = k0 + (p-1)//d
+    
+    return ki
+
+def get_ai(d):
+    
+    a0 = (x1-delta1*k)/gama % ((p-1)/d)
+    ai = a0
+
+    if d == 1:
+          return a0
+    
+    while power_mod(alpha,ai,p)%p != beta:
+        ai = a0 + (p-1)//d
+    
+    return ai
+
+k = get_ki(d)
+print 'k=', k
+print 'ar k geras?', power_mod(alpha,k,p)%p == gama
+
+d = gcd(gama,p-1)
+
+a = get_ai(d)
+print 'a=', a
+print 'ar a geras?', power_mod(alpha,a,p)%p == beta
+
+
+# Desifravimas
 M=C_2/power_mod(C_1,a,p)%p
-M
+print i_teksta(M)
+
+
+          
+    
+    
+    
+    
+    

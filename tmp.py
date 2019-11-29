0d7ae5afbfea36997f5cde100f481b481484fac8
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-29 10:41:32 +0200

    .

---
+++ tmp.py
@@ -0,0 +1,85 @@
+# Tekstams versti skaičiais (ir atvirkščiai)
+A='abcdefghijklmnopqrstuvwxyz'
+def i_skaiciu(text):
+    t=''
+    for r in text:
+        if r in A:
+            ind=A.index(r)+1
+            if ind<10: t=t+'0'+str(ind)
+            else: t=t+str(ind)
+    return int(t,10)  
+
+def i_teksta(M):
+    n=M
+    text=''
+    while n>0:
+        ind=n%100
+        ind=ind-1
+        if (ind>=0) & (ind<len(A)):
+            text+=A[ind]
+            n=(n-ind+1)//100
+        else:
+            text+='?'
+            n=(n-ind+1)//100            
+    return text[::-1]     
+    
+#M=i_skaiciu('vienas')
+#T=i_teksta(M)
+
+
+#Vieši Algio ElGamalio  schemos raktai
+[p,alpha,beta]=[12016012609141909200527091927191118250205121973, 1340, 567229645341541746447797908693212382352016288]
+
+#Algio parašai
+# tekstas_1
+t1="naujienos"
+x1=140121100905141519
+# Parašas
+[gama1,delta1] = [5570393885818468990720736511935245441421473683, 11318193604278953165137689101864251625102398263]
+
+#tekstas_2
+t2 = "lektuvas veluos"
+x2=120511202122011927220512211519
+# Parašas
+[gama2,delta2] = [5570393885818468990720736511935245441421473683, 6600840420895214539082508270820765392182440243]
+
+# Šifruotas laiškas Algiui 
+[C_1,C_2]= [5353434162946175342506864580712183347054386024, 3415125406315829810122511480055610251790431991]
+
+#gama1=gama2
+gama = gama1
+
+#tikriname parasus
+#parasas1
+print(power_mod(beta,gama,p)*power_mod(gama,delta1,p)%p == power_mod(alpha,x1,p))
+#parasas2
+print(power_mod(beta,gama,p)*power_mod(gama,delta2,p)%p == power_mod(alpha,x2,p) %p)
+
+d=gcd(delta1-delta2,p-1)
+
+def get_ki(d):
+    
+    k0 = (x1-x2)/(delta1-delta2) % ((p-1)/d)
+    ki = k0
+
+    if d == 1:
+          return k0
+    
+    while power_mod(alpha,ki,p)%p != gama:
+        ki = k0 + (p-1)/d
+    
+    return ki
+
+k = get_ki(d)
+print k
+
+a = (x1-delta1*k)*(1/gama) % (p-1)
+print a
+
+
+          
+    
+    
+    
+    
+    
